1ada2a9a9d5df4fc31ec53e4f6eb52190c69f27d
afrin32 <32366189+afrin32@users.noreply.github.com>
2017-12-16 01:12:57 +0600

    Add files via upload

---
+++ TomorrowIsEid(DCP-85).cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+        int n;
+         cin>>n;
+    if(n==30 || n==29) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+
+
+    return 0;
+
+
+
+}
+
+
---
+++ chickenFry(DCP-82).cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+            cin>>n;
+    if(n>=120) cout<<"Good Boy Sifat"<<endl;
+    else cout<<"Naughty Boy Sifat"<<endl;
+
+    }
+    return 0;
+
+
+
+}
+
